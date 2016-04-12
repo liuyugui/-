a9a91ff523e54773c475a80bdbf928727ccd6aad
liuyugui <liu_yu_gui@sina.com>
2016-04-12 10:07:19 +0800

    指纹解锁
    指纹解锁

---
+++ zhiwen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhiwen
+//
+//  Created by 法大大 on 16/4/8.
+//  Copyright © 2016年 fadada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zhiwen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zhiwen
+//
+//  Created by 法大大 on 16/4/8.
+//  Copyright © 2016年 fadada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
